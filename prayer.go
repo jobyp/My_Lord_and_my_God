147baf7fbd713ecf58a99d14a650ab4deaf730a9
Joby Poriyath <pcjoby@gmail.com>
2021-11-30 13:07:08 +0000

    Add my prayer.

---
+++ prayer.go
@@ -0,0 +1,17 @@
+// JMJ
+// hello.go
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("My Lord and my God. Thy will be done.")
+	fmt.Println("Jesus, I trust in You!")
+	fmt.Println("Thy will, not mine, be done.")
+	fmt.Println("I give you my will. Please give me yours in return.")
+	fmt.Println("O Jesus, I surrender myself to you, take care of everything!")
+	fmt.Println("Mother, I'm yours now and forever.")
+	fmt.Println("Through you and with you I always want to belong completely to Jesus.")
+}
