a52e361b8b178a54da129e23cefa8a5b678e1c0b
Joby Poriyath <pcjoby@gmail.com>
2022-05-19 19:46:40 +0100

    Build latest tag of musl-libc.

--- c/build-musl-libc.sh
+++ c/build-musl-libc.sh
@@ -6,7 +6,7 @@ pushd $HOME/src
 if [[ -d musl ]]; then
     pushd musl
     git checkout master
-    git pull
+    git pull --tags
     popd
 else
     git clone git://git.musl-libc.org/musl
@@ -14,7 +14,7 @@ fi
 
 pushd musl
 git branch -D release || true
-git checkout -b release v1.2.3
+git checkout -b release $(git describe --tags --abbrev=0)
 make distclean
 [[ -d $HOME/musl ]] && rm -rf $HOME/musl
 ./configure --prefix=$HOME/musl --disable-shared --exec-prefix=$HOME --syslibdir=$HOME/musl/lib
